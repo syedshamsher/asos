7a943242a839226d46c638e54542058774cd8c44
syedshamsher <shamsher78619@gmail.com>
2023-03-08 11:50:47 +0530

    added server-side configuration changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const shoppingRoute = require('./routes/shopping')
-const responseTime = require('response-time');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const shoppingRoute = require("./routes/shopping");
+const responseTime = require("response-time");
 const path = require("path");
 
 const server = express();
@@ -13,23 +13,26 @@ server.use(responseTime());
 dotenv.config();
 
 //connection to dataBase
-mongoose.connect(process.env.MONGO_ATLAS_URI, {
+mongoose.connect(
+  process.env.MONGO_ATLAS_URI,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
-}, (err) => {
-    if(err) {
-        console.log( 'Error connecting to database' );
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err, "Error connecting to database");
+    } else {
+      console.log("Database Successfully connected");
     }
-    else {
-        console.log( 'Database Successfully connected' );
-    }
-})
+  }
+);
 
-server.use('/asos', shoppingRoute)
+server.use("/asos", shoppingRoute);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-    console.log("server is up and running");
-})
+  console.log(`server is up and running at port ${port}`);
+});
